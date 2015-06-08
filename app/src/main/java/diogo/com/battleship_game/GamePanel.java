005be27e5636d308fd227cb74a3dfdf4c30928fc
Diogo Carvalho <diogoavc@gmail.com>
2015-06-08 17:16:21 +0100

    MainThread

--- app/src/main/java/diogo/com/battleship_game/GamePanel.java
+++ app/src/main/java/diogo/com/battleship_game/GamePanel.java
@@ -1,12 +1,11 @@
 package diogo.com.battleship_game;
 
 import android.content.Context;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-/**
- * Created by Diogo on 08/06/2015.
- */
+
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
 {
     public GamePanel (Context context){
@@ -14,5 +13,19 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
 
         //in order to interrupt events with the touch action
         getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){}
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder){}
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder){}
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        return super.onTouchEvent(event);
     }
 }
---
+++ app/src/main/java/diogo/com/battleship_game/MainThread.java
@@ -0,0 +1,53 @@
+package diogo.com.battleship_game;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+/**
+ * Created by Diogo on 08/06/2015.
+ */
+public class MainThread {
+    private SurfaceHolder surfaceHolder;
+    private GamePanel gamePanel;
+    private boolean running;
+    public static Canvas canvas;
+
+    public MainThread(SurfaceHolder surfaceHolder, GamePanel gamePanel)
+    {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gamePanel = gamePanel;
+    }
+
+    @Override
+    public void run()
+    {
+        while(running) {
+            startTime = System.nanoTime();
+            canvas = null;
+
+            //try locking the canvas for pixel editing
+            try {
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {
+                    this.gamePanel.update();
+                    this.gamePanel.draw(canvas);
+                }
+            } catch (Exception e) {
+            } finally {
+                if (canvas != null) {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public void setRunning(boolean bool)
+    {
+        this.running = bool;
+    }
+}
